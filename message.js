67ab5cdd9b88c2ecf158760880c94c3c212ccad0
CJ <cj@68167666.com>
2018-05-06 23:28:15 +0800

    update

--- message.js
+++ message.js
@@ -1,17 +1,8 @@
 ! function () {
   var view = document.querySelector('section.message')
-  var controller = {
-    view: null,
-    messageList: null,
-    init: function (view) {
-      this.view = view
-      this.messageList = view.querySelector('#message')
-      this.form = view.querySelector('#postMessageForm')
-      this.initAV()
-      this.loadMessages()
-      this.bindEvents()
-    },
-    initAV: function () {
+
+  var model = {
+    init: function () {
       var APP_ID = 'Mgyd4BsqnbuXyX3iMpVS6kt4-gzGzoHsz'
       var APP_KEY = 'JmPTXsy7OmaxUNI7co13YIeq'
       AV.init({
@@ -19,9 +10,36 @@
         appKey: APP_KEY
       })
     },
-    loadMessages: function () {
+    fetch: function () {
       var query = new AV.Query('message')
-      query.find().then(function (messages) {
+      return query.find()
+    },
+    save: function (name, content) {
+      var Message = AV.Object.extend('message')
+      var message = new Message()
+      return message.save({
+        'name': name,
+        'content': content
+      })
+    }
+  }
+
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function(view, model) {
+      this.view = view
+      this.model = model
+      this.messageList = view.querySelector('form')
+      this.form = view.querySelector('#postMessageForm')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+
+    loadMessages: function () {
+      this.model.fetch().then((messages) => {
         // 成功获得实例
         // todo 就是 id 为 57328ca079bc44005c2472d0 的 Todo 对象实例
         let array = messages.map((item) => item.attributes)
@@ -34,7 +52,7 @@
       })
     },
     bindEvents: function () {
-      this.form.addEventListener('submit', function (e) {
+      this.form.addEventListener('submit',  (e)=> {
         e.preventDefault()
         this.saveMessage()
       })
@@ -43,20 +61,15 @@
       let myForm = this.form
       let name = myForm.querySelector('input[name=name]').value
       let content = myForm.querySelector('input[name=content]').value
-      var Message = AV.Object.extend('message')
-      var message = new Message()
-      message.save({
-        'name': name,
-        'content': content
-      }).then(function (object) {
+      this.model.save(name, content).then((object) => {
         console.log('保存成功')
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}:${object.attributes.content}`
         let messageList = document.querySelector('#messageList')
-        this.messageList.appendChild(li)
+        messageList.appendChild(li)
         myForm.querySelector('input[name=content]').value = ''
       })
     }
   }
-  controller.init(view)
+  controller.init(view,model)
 }.call()
